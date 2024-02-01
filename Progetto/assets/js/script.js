df75aa57f754486aa3c214310dc7b8d115211a56
Filippo <corti.filippo03@gmail.com>
2024-02-01 12:08:26 +0100

    Grid Layout + Header

--- Progetto/assets/js/script.js
+++ Progetto/assets/js/script.js
@@ -1,6 +1,37 @@
+/* Build GRID: */
+
+let grid_container = document.querySelector(".grid");
+let grid_col = document.querySelector(".grid-col");
+const COLS = 30;
+const COL_SIZE = window.screen.width / COLS;
+let grid_row = document.querySelector(".grid-row");
+const ROW_SIZE = COL_SIZE;
+const ROWS = document.body.scrollHeight / ROW_SIZE;
+
+for (let i = 1; i < COLS; i++) {
+    let new_col = grid_col.cloneNode(true);
+    new_col.classList.remove("d-none");
+    new_col.style.left = COL_SIZE * i + "px";
+    grid_container.appendChild(new_col);
+}
+
+for (let i = 1; i < ROWS; i++) {
+    let new_row = grid_row.cloneNode(true);
+    new_row.classList.remove("d-none");
+    new_row.style.top = ROW_SIZE * i + "px";
+    grid_container.appendChild(new_row);
+}
+
+
+
+
+
+
 async function showRandomMeals() {
     let meals = await fetchRandomMeals(6);
     for (let meal of meals) {
         console.log(extractIngredients(meal))
     }
 }
+
+
---
+++ Progetto/fonti.md
@@ -0,0 +1,16 @@
+
+### Background Grid
+
+https://stripesessions.com/#EventOverview
+
+
+### Icon Coloring
+
+https://stackoverflow.com/questions/7415872/change-color-of-png-image-via-css
+https://caniuse.com/css-filters
+https://codepen.io/sosuke/pen/Pjoqqp
+
+### Document Height
+
+https://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript
+https://caniuse.com/mdn-api_element_scrollheight
